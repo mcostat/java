0371d980403559c76cfc439f59a4cc46c29de278
Mateus <mcostat@outlook.com>
2024-02-03 21:12:19 +0000

    added order status

--- demo/src/main/java/com/example/udemy/demo/entities/Order.java
+++ demo/src/main/java/com/example/udemy/demo/entities/Order.java
@@ -13,23 +13,25 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order  implements Serializable {
-    
+public class Order implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant data;
-    
+
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
-    public Order(Long id, Instant data, User client){
+    public Order(Long id, Instant data, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.data = data;
         this.client = client;
-        
+        this.setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -46,6 +48,15 @@ public class Order  implements Serializable {
 
     public void setData(Instant data) {
         this.data = data;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) 
+            this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
