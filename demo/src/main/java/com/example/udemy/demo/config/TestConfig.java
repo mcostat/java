6963182b648ec4791d81ff538fd253f389826b2f
Mateus <mcostat@outlook.com>
2024-02-03 19:42:12 +0000

    added findbyid

---
+++ demo/src/main/java/com/example/udemy/demo/config/TestConfig.java
@@ -0,0 +1,29 @@
+package com.example.udemy.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.example.udemy.demo.entities.User;
+import com.example.udemy.demo.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @SuppressWarnings("null")
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+
+    }
+
+}
--- demo/src/main/java/com/example/udemy/demo/resources/UserResource.java
+++ demo/src/main/java/com/example/udemy/demo/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.example.udemy.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.udemy.demo.entities.User;
+import com.example.udemy.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "null", "null", "11111111", "password");
+    @Autowired
+    private UserService service;
 
-        return ResponseEntity.ok().body(u);
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(user);
     }
 }
