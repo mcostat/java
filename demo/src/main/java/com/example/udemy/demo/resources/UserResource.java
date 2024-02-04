c79d4080c6ede34e19155fc7d6e9f50251414db8
Mateus <mcostat@outlook.com>
2024-02-04 23:37:58 +0000

    create user

--- demo/src/main/java/com/example/udemy/demo/resources/UserResource.java
+++ demo/src/main/java/com/example/udemy/demo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.example.udemy.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.udemy.demo.entities.User;
 import com.example.udemy.demo.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -32,4 +37,12 @@ public class UserResource {
 
         return ResponseEntity.ok().body(obj);
     }
+
+    @PostMapping
+    public ResponseEntity<User> insertUser(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+    
 }
